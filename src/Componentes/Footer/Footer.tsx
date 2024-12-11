4dfab835e66e561e4c414c55e50ffba3a197481f
Fernando <fernandorgoncalves@hotmail.com>
2024-12-11 14:24:26 -0300

    Adicionando o footer

--- src/Componentes/Footer/Footer.tsx
+++ src/Componentes/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import styles from './Footer.module.css'
+
 
 import logo from '../../assets/logo.svg'
 import x from '../../assets/icn-twiter.svg'
@@ -8,20 +10,43 @@ import linkedin from '../../assets/icn-linkedin.svg'
 export function Footer(){
     return(
         <div>
-            <div>
-                <img src={logo} alt="logo"/>
+            <div className={styles.containerFooter}>
+
+                <div className={styles.logoFooter}>
                 <div>
-                    <img src={x} alt="logo"/>
-                    <img src={instagram} alt="logo"/>
-                    <img src={youtube} alt="logo"/>
-                    <img src={linkedin} alt="logo"/>
+                <img src={logo} alt="logo" className={styles.logo}/>
+                </div>
+
+                <div className={styles.redes}>
+                    <img src={x} alt="twitter"/>
+                    <img src={instagram} alt="instagram"/>
+                    <img src={youtube} alt="youtube"/>
+                    <img src={linkedin} alt="linkedin"/>
+                </div>
+                </div>
+
+                <div className={styles.footerlinks}>
+            <div>
+                <div>
+                    <ul>
+                <h3>Partinership</h3> 
+                        <li>Beneficios</li>
+                        <li>Sobre nós</li>
+                        <li>Contato</li>
+                    </ul>
                 </div>
             </div>
             <div>
-
+                <h3>Acesso</h3>
+                <div>
+                    <ul>
+                        <li>Registre-se</li>
+                        <li>Login</li>
+                        <li className={styles.ponto}>.</li>
+                    </ul>
+                </div>
             </div>
-            <div>
-
+            </div>
             </div>
         </div>
     
